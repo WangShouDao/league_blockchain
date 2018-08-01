857187591b7e756813228168ecda875d215ac5ad
WangShouDao <1248773432@qq.com>
2018-08-01 21:10:42 +0800

    first commit

---
+++ src/main/java/com/league/blockchain/ApplicationContextProvider.java
@@ -0,0 +1,12 @@
+package com.league.blockchain;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextProvider {
+
+}
---
+++ src/main/java/com/league/blockchain/block/Operation.java
@@ -0,0 +1,7 @@
+package com.league.blockchain.block;
+
+public interface Operation {
+    byte ADD = 1;
+    byte DELETE = -1;
+    byte UPDATE = 2;
+}
